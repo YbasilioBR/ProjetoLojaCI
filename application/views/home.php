05268dff4578baf6dccfe18d4d5799903d00889f
Ygor Basilio <ygornunes@finchsolucoes.com.br>
2018-05-15 13:42:58 -0300

    atualizações

---
+++ application/views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Loja de Mangás - HOME</title>
+</head>
+<body>
+    
+
+
+</body>
+</head>
+</html>
---
+++ application/views/produto/inserir.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+        <body>
+            <h1>Cadastro de produtos</h1>
+            
+            <input
+
+
+        </body>
+    </head>
+</html>
