99e4420ac8cd6338d0a796d7ef13b38d992940df
Ygor Basilio <ygornunes@finchsolucoes.com.br>
2018-05-15 13:46:30 -0300

    atualizações

--- application/controllers/Produto.php
+++ application/controllers/Produto.php
@@ -5,22 +5,32 @@ class Produto extends CI_Controller {
 
         public function index()
 	{
-               $texto = "Você está usando o controller produtos";
-
-               $dados = array("mensagem" => $texto);
-
-               $this->load->view("produto/lista", $dados);
+                $texto = "Você está usando o controller produtos";
+                $dados = array("mensagem" => $texto);
+                $this->load->view("produto/lista", $dados);
 	}
 
 	public function listar_array()
 	{
-                $this->load->model("ProdutoModel","produto");
-        
-                $resultado = $this->produto->listar_todos();
-
+                $this->load->model("ProdutoModel","produto");        
+                $resultado = $this->produto->listar_produtos();
                 echo '<pre>';
-
                 var_dump($resultado);
-	}
+        }
 
+        public function inserir_produtos(){
+
+                $this->load->view("produto/inserir");
+                $this->load->model("ProdutoModel","produto");
+
+                $descricao = $this->input->post('descricao');
+                $valor = $this->input->post('valor');
+
+                $this->produto->inserir_produtos([
+                        
+                        'descricao' => $descricao,
+                        'valor' => $valor  
+                ]);
+        }
+        
 }
--- application/models/ProdutoModel.php
+++ application/models/ProdutoModel.php
@@ -3,13 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ProdutoModel extends CI_Model{
     
-    public function listar_todos()
+    public function listar_produtos()
     {
-        $this->db->select("id_produto, descricao, valor");
-        
+        $this->db->select("id_produto, descricao, valor");        
 	    $resultado = $this->db->get("produto")->result();
-
 	    return $resultado;
-
     }
+
+    public function inserir_produtos($data){
+        $this->db->insert("produto",$data);
+        
+    }
+    
 }
