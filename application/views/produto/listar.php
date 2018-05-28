6f13df5882d0b6ebded20489eee493de05f3747b
Ygor Basilio <ygornunes@finchsolucoes.com.br>
2018-05-28 14:19:54 -0300

    atualizações 28/05

--- application/views/produto/listar.php
+++ application/views/produto/listar.php
@@ -19,109 +19,75 @@
     </script>
 </head>
 <body class="fundo">
-    
-    <div class="cabecalho"></div>
-    <nav class="menu">			
-            <ul>
-            <li><a href="#" title="Home">Home</a></li>
-            <li><a href="#" title="Titulos">Titulos</a></li>
-            <li><a href="#" title="Usados">Usados</a></li>
-            <li><a href="#" title="Venda">Venda</a></li>
-            <li><a href="#" title="Troca">Troca</a></li>
-            </ul>
-    </nav>
     <form id="lista" method="get">
     <div class="conteudo">
         <div class="dentroconteudo">
-            <div class="container"> 
-                <div class="box">
-                    <ul>
-                        <li>
-                            <img src="../../imagens/VOL40.jpg" alt="Smiley face">
-                        </li>
-                        <li>
-                        <div class="texto-item">
-                            <a href=""> 
-                                Teste de texto
-                            </a>
-                        </div>
-                        </li>
-                    </ul>
-                </div> 
-                <div class="box">
-                    <ul>
-                        <li>
-                            <img src="../../imagens/VOL40.jpg" alt="Smiley face">
-                        </li>
-                        <li>
-                        <div class="texto-item">
-                            <a href=""> 
-                                Teste de texto
-                            </a>
-                        </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="box">
-                    <ul>
-                        <li>
-                            <img src="../../imagens/VOL40.jpg" alt="Smiley face">
-                        </li>
-                        <li>
-                        <div class="texto-item">
-                            <a href=""> 
-                                Teste de texto
-                            </a>
-                        </div>
-                        </li>
-                    </ul>
-                </div>            
+            <div class="cabecalho"></div>
+            <nav class="menu">			
+                <ul>
+                    <li><a href="#" title="Home"></a></li>
+                    <li><a href="#" title="Home">Home</a></li>
+                    <li><a href="#" title="Titulos">Titulos</a></li>
+                    <li><a href="#" title="Usados">Usados</a></li>
+                    <li><a href="#" title="Venda">Venda</a></li>
+                    <li><a href="#" title="Troca">Troca</a></li>
+                </ul>
+            </nav>
+                <div class="container"> 
+                    <div class="box">
+                        <ul>
+                            <li>
+                                <img src="../../imagens/VOL40.jpg" alt="Smiley face">
+                            </li>
+                            <li>
+                            <div class="texto-item">
+                                <a href=""> 
+                                    Teste de texto
+                                </a>
+                            </div>
+                            <div class="texto-item">
+                                   R$ 99,99
+                            </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="box">
+                        <ul>
+                            <li>
+                                <img src="../../imagens/VOL40.jpg" alt="Smiley face">
+                            </li>
+                            <li>
+                            <div class="texto-item">
+                                <a href=""> 
+                                    Teste de texto
+                                </a>
+                            </div>
+                            <div class="texto-item">
+                                   R$ 99,99
+                            </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="box">
+                        <ul>
+                            <li>
+                                <img src="../../imagens/VOL40.jpg" alt="Smiley face">
+                            </li>
+                            <li>
+                            <div class="texto-item">
+                                <a href=""> 
+                                    Teste de texto
+                                </a>
+                            </div>
+                            <div class="texto-item">
+                                   R$ 99,99
+                            </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>                           
             </div>
-
-            <div class="container"> 
-                <div class="box">
-                    <ul>
-                        <li>
-                            <img src="../../imagens/VOL40.jpg" alt="Smiley face">
-                        </li>
-                        <li>
-                        <div class="texto-item">
-                            <a href=""> 
-                                Teste de texto
-                            </a>
-                        </div>
-                        </li>
-                    </ul>
-                </div> 
-                <div class="box">
-                    <ul>
-                        <li>
-                            <img src="../../imagens/VOL40.jpg" alt="Smiley face">
-                        </li>
-                        <li>
-                        <div class="texto-item">
-                            <a href=""> 
-                                Teste de texto
-                            </a>
-                        </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="box">
-                    <ul>
-                        <li>
-                            <img src="../../imagens/VOL40.jpg" alt="Smiley face">
-                        </li>
-                        <li>
-                        <div class="texto-item">
-                            <a href=""> 
-                                Teste de texto
-                            </a>
-                        </div>
-                        </li>
-                    </ul>
-                </div>            
-            </div>
+            <div class="rodape"></div>
         </div>
     </form>
     </div>
